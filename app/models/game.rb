6fefa4f219eef0c96fe957352f68bb5d5122e7f6
Anthony Hill <hillganthony@outlook.com>
2021-11-10 10:05:43 -0500

    commit tree 48f62cc8c239cfe37d83a6f24432a1bab55adb75

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,10 @@
 class Game < ActiveRecord::Base
-  
+has_many :reviews
+
+    def reviews
+        Review.where(game_id: id)
+        game.reviews << Review.create(score: 4, comment: "it's alright I guess")
+
+    end
+
 end
